249d93f14b8f1cfcb9048505546c29461cc88b3e
Layrane Leão <contato.layrane@gmail.com>
2025-07-31 20:05:21 -0300

    Subindo o desafio pro GitHub

---
+++ README.md
@@ -0,0 +1,12 @@
+﻿# Desafio Número Secreto
+Projeto desenvolvido como parte do curso **"Lógica de Programação: Mergulhe em Programação com JavaScript"**, oferecido pela **Alura** em parceria com a **Oracle**, na formação **ONE - Oracle Next Education.**
+
+## Tecnologias usadas:
+- HTML
+- CSS (com foco em **Flexbox**)
+- JavaScript
+
+## Sobre o projeto:
+Este desafio teve como objetivo reforçar os conceitos de lógica de programação utilizando JavaScript, com foco na geração de números aleatórios e estrutura de condicionais.
+
+Para ir além da proposta original, desenvolvi também a interface visual do jogo, aplicando **Flexbox** no layout com CSS, um dos tópicos que estou estudando no momento. Aproveitei para praticar a integração entre **HTML e JavaScript**, manipulando elementos via `getElementById()` e atualizando o conteúdo dinamicamente.
---
+++ script.js
@@ -0,0 +1,25 @@
+﻿let numeroSecreto = Math.floor(Math.random() * 11); // número entre 0 e 10
+let quantidadeTentativas = 5;
+
+function enviarNumero() {
+    let valorDigitado = parseInt(document.getElementById("numero-aleatorio").value);
+    let mensagem;
+    let mensagemDica = '';
+
+    if (isNaN(valorDigitado) || valorDigitado < 0 || valorDigitado > 10) {
+        mensagem = 'Você não digitou um número válido! Digite um número entre 0 e 10.';
+    } else {
+        if (valorDigitado === numeroSecreto) {
+            mensagem = `Parabéns, você acertou! O número secreto é ${numeroSecreto}.`;
+        } else {
+            mensagem = (quantidadeTentativas > 0) ? `Você errou o número secreto!\nVocê tem apenas mais ${quantidadeTentativas} tentativa(s).` : `Você perdeu! O número secreto era ${numeroSecreto}.`;
+            mensagemDica = (valorDigitado > numeroSecreto) ? "Dica: O número secreto é MENOR!" : "Dica: O número secreto é MAIOR!";
+
+            quantidadeTentativas--;
+        }
+    }
+
+    document.getElementById("saida").textContent = mensagem;
+    document.getElementById("saida-dica").textContent = mensagemDica;
+
+}
